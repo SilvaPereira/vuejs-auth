54671c11173d0a4103fb775e13ba5976a13d5079
unknown <andre_silva6@hotmail.com>
2020-08-25 13:05:36 -0300

    finishing auth login methods related

--- src/store.js
+++ src/store.js
@@ -3,21 +3,36 @@ import Vuex from 'vuex'
 import axios from './axios-auth'
 import globalAxios from 'axios'
 
+import router from './router'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     idToken: null,
-    userId: null
+    userId: null,
+    user: null
   },
   mutations: {
     authUser (state, userData) {
       state.idToken = userData.token,
       state.userId = userData.userId
+    },
+    storeUser (state, user){
+      state.user = user
+    },
+    clearAuthData (state) {
+      state.idToken = null
+      state.userId = null
     }
   },
   actions: {
-    signup ({commit}, authData) {
+    setLogoutTimer ({commit}, expirationTime) {
+      setTimeout( () => {
+        commit('clearAuthData')
+      }, expirationTime * 1000) 
+    },
+    signup ({commit, dispatch}, authData) {
       axios.post('/accounts:signUp?key=AIzaSyDipzqtuBMkWRlumG3Bof-Kv75ouHZataI', {
         email: authData.email,
         password: authData.password,
@@ -29,10 +44,17 @@ export default new Vuex.Store({
            token: res.data.idToken,
            userId: res.data.localId  
           })
+          const now = new Date()
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+          localStorage.setItem('token', res.data.idToken)
+          localStorage.setItem('userId', res.data.localId)
+          localStorage.setItem('expirationDate', expirationDate)
+          dispatch('storeUser', authData)
+          dispatch('setLogoutTimer', res.data.expiresIn)
         })
         .catch(error => console.log(error))
     },
-    login ({commit}, authData) {
+    login ({commit, dispatch}, authData) {
       axios.post('/accounts:signInWithPassword?key=AIzaSyDipzqtuBMkWRlumG3Bof-Kv75ouHZataI', {
         email: authData.email,
         password: authData.password,
@@ -40,20 +62,55 @@ export default new Vuex.Store({
       })
       .then(res => {
         console.log(res)
+        const now = new Date()
+        const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000)
+        localStorage.setItem('token', res.data.idToken)
+        localStorage.setItem('userId', res.data.localId)
+        localStorage.setItem('expirationDate', expirationDate)
         commit('authUser', {
          token: res.data.idToken,
          userId: res.data.localId  
         })
+        dispatch('setLogoutTimer', res.data.expiresIn)
       })
         .catch(error => console.log(error))
     },
+    tryAutoLogin({commit}) {
+      const token = localStorage.getItem('token')
+      if (!token){
+        return
+      }
+      const expirationDate = localStorage.getItem('expirationDate')
+      const now = new Date()
+      if (now >= expirationDate) {
+        return
+      } 
+      const userId = localStorage.getItem('userId')
+      commit('authUser', {
+        token: token,
+        userId: userId
+      })
+    },
+    logout ({commit}) {
+      commit('clearAuthData')
+      localStorage.removeItem('expirationDate')
+      localStorage.removeItem('token')
+      localStorage.removeItem('userId')
+      router.replace('/signin')
+    },
     storeUser ({commit}, userData) {
-      globalAxios.post('/users.json', userDatas)
+      if (!state.idToken) {
+        return
+      }
+      globalAxios.post('/users.json' + '?auth=' + state.idToken, userData)
       .then(res => console.log(res))
       .catch(error => console.log(error))
     },
-    fetchUser ({commit}) {
-      axios.get('/users.json')
+    fetchUser ({commit, state}) {
+      if (!state.idToken) {
+        return
+      }
+      globalAxios.get('/users.json' + '?auth=' + state.idToken)
       .then(res => {
         coonsole.log(res)
         const data = res.data
@@ -64,12 +121,17 @@ export default new Vuex.Store({
           users.push(user)
         }
         console.log(users)
-        this.email = users[0].email
+        commit('storeUser', users[0])
       })
       .catch(error => console.log(error))
     }
   },
   getters: {
-
+    user (state) {
+      return state.user
+    },
+    isAuthenticated (state) {
+      return state.idToken !== null
+    }
   }
 })
